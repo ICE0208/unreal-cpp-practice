ecccc40680885591e5a6cd4869d35d2f9fb3325b
ICE0208 <doky3307@naver.com>
2022-06-15 12:27:13 +0900

    Initial commit

---
+++ CppPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPPRACTICE_API ACppPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
